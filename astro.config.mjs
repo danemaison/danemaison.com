482a5e21caf3319cad47c03b1c98eef717d617c5
Dane Maison <dane.maison@wildfire-corp.com>
2026-04-10 21:34:16 -0700

    revert baseurl change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://danemaison.github.io',
-  base: '/danemaison.com',
-  output: 'static',
+  site: "https://danemaison.com",
+  output: "static",
 });
